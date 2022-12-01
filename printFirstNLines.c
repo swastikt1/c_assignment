0bf4e08dc3f5c0764d182fd2c0b6e2c9dd7b8148
swastikt1 <swastikt1@gmail.com>
2022-12-01 14:42:30 +0530

    major changes

--- printFirstNLines.c
+++ printFirstNLines.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,20 +7,23 @@ char ** firstnumlines(FILE * inputfp, unsigned int num){
 	char ** headbuff = (char **) calloc(num, sizeof(char *));
 	int i=0;
 	//char c;
-	while( i<num ){
+	while(i<num ){
 		//c = fgetc(inputfp);
-		if(feof(inputfp)){break;}
+		if(feof(inputfp)){
+				printf("EOF REACHED\n\n");
+				break;
+			}
 		headbuff[i] = (char *) calloc(255, sizeof(char));
 		fgets(headbuff[i],255,inputfp);
 		/*if(headbuff[i] != NULL){
 		printf("%s",headbuff[i]);
-	    }*/
+	    }*/	
 		//c = getc(inputfp);
 		i++;
 		//free(headbuff[i]);
 	}
 	return headbuff;
-
+	
 }
 
 
@@ -34,7 +36,7 @@ int main() {
 	char ** firstlines = firstnumlines(inputfp,num);
 
     //firstnumlines(inputfp,num);
-
+	
 	for(int i=0;i<num;i++){
 		if(firstlines[i] != NULL){
 			printf("%s",firstlines[i]);
@@ -42,7 +44,7 @@ int main() {
 	}
 
 
-/*
+/*	
  	for(int i =0;i<num;i++){
 		free(firstlines[i]);
 		}
@@ -52,3 +54,4 @@ int main() {
 	//printf("\n");
 	fclose(inputfp);
 }
+
