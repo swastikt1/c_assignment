89b885a18ae9e35cbc60aaf59659befb053d43ca
swastikt1 <swastikt1@gmail.com>
2022-12-01 10:51:36 +0530

    first commit

---
+++ printFirstNLines.c
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//char ** firstnumlines(FILE * inputfp, unsigned int num){
+char ** firstnumlines(FILE * inputfp, unsigned int num){
+	char ** headbuff = (char **) calloc(num, sizeof(char *));
+	int i=0;
+	//char c;
+	while(!(feof(inputfp)) || i<num ){
+		//c = fgetc(inputfp);
+		headbuff[i] = (char *) calloc(255, sizeof(char));
+		fgets(headbuff[i],255,inputfp);
+		/*if(headbuff[i] != NULL){
+		printf("%s",headbuff[i]);
+	    }*/
+		//c = getc(inputfp);
+		i++;
+		//free(headbuff[i]);
+	}
+	return headbuff;
+
+}
+
+
+
+int main() {
+	FILE * inputfp = fopen("input.txt", "r");
+
+	int num;
+	scanf("%d",&num);
+	char ** firstlines = firstnumlines(inputfp,num);
+
+    //firstnumlines(inputfp,num);
+
+	for(int i=0;i<num;i++){
+		if(firstlines[i] != NULL){
+			printf("%s",firstlines[i]);
+		}
+	}
+
+
+/*
+ 	for(int i =0;i<num;i++){
+		free(firstlines[i]);
+		}
+*/
+
+	free(firstlines);
+	//printf("\n");
+	fclose(inputfp);
+}
